961b3cea5c9d6520600516414dc441981788dbc4
Sahil Mutneja <sahil.mutneja26@gmail.com>
2015-08-03 15:52:30 +0530

    Added Buttons for Playing

--- app/src/main/java/com/sahilmutneja/musicplayer/MainActivity.java
+++ app/src/main/java/com/sahilmutneja/musicplayer/MainActivity.java
@@ -1,17 +1,95 @@
 package com.sahilmutneja.musicplayer;
 
-import android.support.v7.app.ActionBarActivity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private Button mPlayButton;
+    private Button mPauseButton;
+    private Button mFastForwardButton;
+    private Button mRewindButton;
+    private Button mCompletedButton;
+
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.abc);
+
+        mPlayButton = (Button) findViewById(R.id.activity_main_play);
+        mPauseButton = (Button) findViewById(R.id.activity_main_pause);
+
+        mFastForwardButton = (Button) findViewById(R.id.activity_main_fast_forward);
+        mRewindButton = (Button) findViewById(R.id.activity_main_rewind);
+        mCompletedButton = (Button) findViewById(R.id.activity_main_completed);
+
+        mPlayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Play is clicked", Toast.LENGTH_SHORT).show();
+                mediaPlayer.start();
+
+            }
+        });
+
+        mPauseButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast.makeText(MainActivity.this, "Pause is clicked", Toast.LENGTH_SHORT).show();
+                mediaPlayer.pause();
+
+            }
+        });
+
+        mFastForwardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Fast Forward button is clicked", Toast.LENGTH_SHORT).show();
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() + 2000);
+
+            }
+        });
+
+        mRewindButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "Rewind Button is clicked", Toast.LENGTH_SHORT).show();
+                mediaPlayer.seekTo(mediaPlayer.getCurrentPosition() - 2000);
+            }
+        });
+
+        mCompletedButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Toast.makeText(MainActivity.this, "Song is Completed", Toast.LENGTH_SHORT).show();
+                if(mediaPlayer.getCurrentPosition() == mediaPlayer.getDuration()){
+                    Toast.makeText(MainActivity.this, "Song is Completed", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(MainActivity.this, "Song is Not Completed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                Toast.makeText(MainActivity.this, "Song is Completed/Completion Listener", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
